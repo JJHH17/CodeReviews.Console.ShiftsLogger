260d2ecd07a393d49df52f89700d3c3aca7047ae
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 16:51:27 +0100

    Added API endpoints and definitions

--- ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Controllers/ShiftsController.cs
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Controllers/ShiftsController.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShiftsLogger.jjhh17.Services;
 
 namespace ShiftsLogger.jjhh17
 {
@@ -12,6 +18,36 @@ namespace ShiftsLogger.jjhh17
         public ShiftsController(IShiftService shiftService)
         {
             _shiftService = shiftService;
+        }
+
+        [HttpGet]
+         public ActionResult<List<Shift>> GetAllShifts(int id)
+         {
+            return Ok(_shiftService.GetAllShifts(id));
+         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Shift> GetShiftById(int id)
+        {
+            return Ok(_shiftService.GetShiftById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<Shift> CreateShift(Shift shift)
+        {
+            return Ok(_shiftService.CreateShift(shift));
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Shift> UpdateShift(int id, Shift updatedShift)
+        {
+            return Ok(_shiftService.UpdateShift(id, updatedShift));
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeleteShift(int id)
+        {
+            return ok(_shiftService.DeleteShift(id));
         }
     }
 }
