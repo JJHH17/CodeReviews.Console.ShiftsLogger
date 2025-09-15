4463f4b315faeeb5a4e8a468c70b1256b9f7c7e5
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 12:49:00 +0100

    Added builder call to shift service interface and class

--- ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Program.cs
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShiftsLogger.jjhh17.Data;
+using ShiftsLogger.jjhh17.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ShiftsDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IShiftService, ShiftService>();
 
 var app = builder.Build();
 
