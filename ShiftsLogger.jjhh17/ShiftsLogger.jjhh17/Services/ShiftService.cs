9dda0263ddbec90795266c0b5de70abf6be3618b
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 12:42:47 +0100

    Created initial ShiftServices interface

---
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Services/ShiftService.cs
@@ -0,0 +1,13 @@
+﻿using ShiftsLogger.jjhh17.Model;
+
+namespace ShiftsLogger.jjhh17.Services
+{
+    public interface ShiftService
+    {
+        public List<Shift> GetAllShifts();
+        public Shift? GetShiftById(int id);
+        public Shift CreateShift(Shift shift);
+        public Shift UpdateShift(int id, Shift updatedShift);
+        public string? DeleteShift(int id);
+    }
+}
