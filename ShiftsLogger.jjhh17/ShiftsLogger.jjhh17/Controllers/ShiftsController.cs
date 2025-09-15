9ae00e00734cc9ced6409a178513e8b18d927fb6
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 16:40:59 +0100

    Added API controller

---
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Controllers/ShiftsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ShiftsLogger.jjhh17
+{
+    [ApiController]
+    [Route("api/shift")]
+    //example: http://localhost:5609/api/shift/
+
+    public class ShiftsController : ControllerBase
+    {
+        private readonly IShiftService _shiftService;
+        public ShiftsController(IShiftService shiftService)
+        {
+            _shiftService = shiftService;
+        }
+    }
+}
