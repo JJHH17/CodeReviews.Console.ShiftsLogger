961be79e4e2807fd0f6d24824062d89945e8bc15
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 11:43:06 +0100

    Added basic controller support

--- ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Program.cs
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -13,5 +14,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.Run();
