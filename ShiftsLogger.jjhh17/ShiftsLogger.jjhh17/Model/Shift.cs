5ae44400edcd2ae4b8db727b079326dfd7b94de2
James Hatfield <james.hatfield17@icloud.com>
2025-09-15 12:11:07 +0100

    Created class constructor for Shifts

---
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Model/Shift.cs
@@ -0,0 +1,12 @@
+﻿namespace ShiftsLogger.jjhh17.Model
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ClockIn { get; set; }
+        public DateTime ClockOut { get; set; }
+        public string Department { get; set; }
+        public int Duration { get; set; }
+    }
+}
