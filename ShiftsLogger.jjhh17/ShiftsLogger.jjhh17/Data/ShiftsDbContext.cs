4192cd5a586218034efb4ba09ea29c2a7eae21cc
James Hatfield <james.hatfield17@icloud.com>
2025-09-16 16:30:49 +0100

    Added dbContext reference

--- ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Data/ShiftsDbContext.cs
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Data/ShiftsDbContext.cs
@@ -3,11 +3,11 @@ using ShiftsLogger.jjhh17.Model;
 
 namespace ShiftsLogger.jjhh17.Data
 {
-    public class ShiftsDbContext: DbContext
+    public class ShiftsDbContext : DbContext
     {
         public ShiftsDbContext(DbContextOptions options) : base(options)
         {
-
+          
         }
 
         public DbSet<Shift> Shifts { get; set; }
