a048f86b480df16f93487cad2d11a60021808e23
James Hatfield <james.hatfield17@icloud.com>
2025-09-16 16:31:09 +0100

    Added model for calculation the duration between sessions

--- ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Model/Shift.cs
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Model/Shift.cs
@@ -1,12 +1,28 @@
-﻿namespace ShiftsLogger.jjhh17.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShiftsLogger.jjhh17.Model
 {
     public class Shift
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime ClockIn { get; set; }
-        public DateTime ClockOut { get; set; }
+        public TimeSpan ClockIn { get; set; }
+        public TimeSpan ClockOut { get; set; }
         public string Department { get; set; }
-        public int Duration { get; set; }
+
+        [NotMapped]
+        public string Duration
+        {
+            get
+            {
+                var duration = ClockOut - ClockIn;
+                if (duration.TotalMinutes < 0)
+                {
+                    duration += TimeSpan.FromDays(1);
+                }
+
+                return duration.ToString(@"hh\:mm");
+            }
+        }
     }
 }
