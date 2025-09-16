13ae97734554b023132f324812f57a6f18b4dd21
James Hatfield <james.hatfield17@icloud.com>
2025-09-16 16:30:04 +0100

    Updated Migrations to includes altered data types for class

---
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Data/Migrations/20250916145121_AddShiftDuration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShiftsLogger.jjhh17.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShiftDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Shifts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Duration",
+                table: "Shifts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ ShiftsLogger.jjhh17/ShiftsLogger.jjhh17/Data/Migrations/20250916150331_AdjustedClockInAndOutTimeSpan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShiftsLogger.jjhh17.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedClockInAndOutTimeSpan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ClockOut",
+                table: "Shifts",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ClockIn",
+                table: "Shifts",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ClockOut",
+                table: "Shifts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ClockIn",
+                table: "Shifts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+    }
+}
